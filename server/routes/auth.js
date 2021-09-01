bd819f7dfaaaf3764a0d087a18bbefa044bf4c15
Jameson Hale <halejameson@gmail.com>
2021-08-31 18:18:37 -0700

    added user and auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,7 +26,7 @@ router.get("/register", async (req, res) => {
 router.post('/register', async (req, res) => {
 
     try {
-        // create hashed password here
+        // create hashed password
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
@@ -38,11 +38,31 @@ router.post('/register', async (req, res) => {
         });
 
         const user = newUser.save();
-        res.status(200).send(newUser);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            res.status(404).json("User not found");
+        }
+
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            res.status(400).json("Wrong password");
+        }
+
+        else res.status(200).json("Successfully logged in:");
     } catch (err) {
         console.log(err);
-        res.send();
     }
+
 });
 
 module.exports = router;
